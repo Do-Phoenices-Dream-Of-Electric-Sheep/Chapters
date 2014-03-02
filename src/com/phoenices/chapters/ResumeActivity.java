5701d9b6235e05c4bc6aadcec76d87afe7320f50
Jonathan Jin <jjin082693@gmail.com>
2014-03-01 20:47:09 -0600

    clean stuff up

--- src/com/phoenices/chapters/ResumeActivity.java
+++ src/com/phoenices/chapters/ResumeActivity.java
@@ -4,18 +4,18 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class ContinueActivity extends Activity {
+public class ResumeActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_continue);
+		setContentView(R.layout.activity_resume);
 	}
 
 	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.continue, menu);
+        getMenuInflater().inflate(R.menu.resume, menu);
         return true;
     }
 }
