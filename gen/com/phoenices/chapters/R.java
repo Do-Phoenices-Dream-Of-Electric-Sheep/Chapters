12d1ce9103bd55144b72551aafb5e61f44fef823
Jonathan Jin <jjin082693@gmail.com>
2014-03-02 12:05:10 -0600

    cleaning and renaming

--- gen/com/phoenices/chapters/R.java
+++ gen/com/phoenices/chapters/R.java
@@ -46,13 +46,13 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050005;
-        public static final int activity_resume_header=0x7f050004;
-        public static final int activity_select_header=0x7f050006;
         public static final int app_name=0x7f050000;
         public static final int btn_newgame=0x7f050001;
         public static final int btn_resume=0x7f050002;
         public static final int btn_settings=0x7f050003;
-        public static final int hello_world=0x7f05000b;
+        public static final int header_activity_resume=0x7f050004;
+        public static final int header_activity_select=0x7f050006;
+        public static final int header_activity_settings=0x7f05000b;
         public static final int title_activity_continue=0x7f050009;
         public static final int title_activity_gameplay=0x7f050007;
         public static final int title_activity_select=0x7f050008;
--- src/com/phoenices/chapters/WelcomeActivity.java
+++ src/com/phoenices/chapters/WelcomeActivity.java
@@ -1,3 +1,4 @@
+
 package com.phoenices.chapters;
 
 import android.os.Bundle;
@@ -8,58 +9,58 @@ import android.view.View;
 import android.widget.Button;
 
 public class WelcomeActivity extends Activity {
-	
-	private Button bNewGame;
-	private Button bResume;
-	private Button bSettings;
-	
-	private View.OnClickListener ListenerNewGame = new View.OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			Intent intentState = new Intent(WelcomeActivity.this, ResumeActivity.class);
-			
-			startActivity(intentState);
-			return;
-		}
-	};
-	
-	private View.OnClickListener ListenerResume = new View.OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			Intent intentState = new Intent(WelcomeActivity.this, ResumeActivity.class);
-			
-			startActivity(intentState);
-			return;
-		}
-	};
-	
-	private View.OnClickListener ListenerSettings = new View.OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			Intent intentState = new Intent(WelcomeActivity.this, SettingsActivity.class);
-			
-			startActivity(intentState);
-			return;			
-		}
-	};
-	
+
+    private Button bNewGame;
+    private Button bResume;
+    private Button bSettings;
+
+    private View.OnClickListener ListenerNewGame = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intentSelect = new Intent(WelcomeActivity.this,
+                    SelectActivity.class);
+
+            startActivity(intentSelect);
+            return;
+        }
+    };
+
+    private View.OnClickListener ListenerResume = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intentResume= new Intent(WelcomeActivity.this,
+                    ResumeActivity.class);
+
+            startActivity(intentResume);
+            return;
+        }
+    };
+
+    private View.OnClickListener ListenerSettings = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intentSettings = new Intent(WelcomeActivity.this,
+                    SettingsActivity.class);
+
+            startActivity(intentSettings);
+            return;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        setContentView(R.layout.activity_welcome);
-        
-		bNewGame = (Button) findViewById(R.id.btn_newgame);
-		bResume = (Button) findViewById(R.id.btn_resume);
-		bSettings = (Button) findViewById(R.id.btn_settings);
-		
-		bNewGame.setOnClickListener(ListenerNewGame);
-		bResume.setOnClickListener(ListenerResume);
-		bSettings.setOnClickListener(ListenerSettings);
-    }
-    
-    
 
+        setContentView(R.layout.activity_welcome);
+
+        bNewGame = (Button) findViewById(R.id.btn_newgame);
+        bResume = (Button) findViewById(R.id.btn_resume);
+        bSettings = (Button) findViewById(R.id.btn_settings);
+
+        bNewGame.setOnClickListener(ListenerNewGame);
+        bResume.setOnClickListener(ListenerResume);
+        bSettings.setOnClickListener(ListenerSettings);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -67,5 +68,5 @@ public class WelcomeActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
 }
