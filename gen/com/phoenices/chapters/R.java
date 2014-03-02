acdca40b0ce99ba532c8e4263aae62950a567da8
Jonathan Jin <jjin082693@gmail.com>
2014-03-01 21:25:30 -0600

    expand activity control flow

--- gen/com/phoenices/chapters/R.java
+++ gen/com/phoenices/chapters/R.java
@@ -24,8 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080004;
         public static final int activity_continue=0x7f080000;
+        public static final int btn_newgame=0x7f080001;
+        public static final int btn_resume=0x7f080002;
+        public static final int btn_settings=0x7f080003;
     }
     public static final class layout {
         public static final int activity_gameplay=0x7f030000;
@@ -40,12 +43,16 @@ public final class R {
         public static final int select=0x7f070003;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
+        public static final int action_settings=0x7f050005;
+        public static final int activity_resume_header=0x7f050004;
+        public static final int activity_select_header=0x7f050006;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int title_activity_continue=0x7f050005;
-        public static final int title_activity_gameplay=0x7f050003;
-        public static final int title_activity_select=0x7f050004;
+        public static final int btn_newgame=0x7f050001;
+        public static final int btn_resume=0x7f050002;
+        public static final int btn_settings=0x7f050003;
+        public static final int title_activity_continue=0x7f050009;
+        public static final int title_activity_gameplay=0x7f050007;
+        public static final int title_activity_select=0x7f050008;
     }
     public static final class style {
         /** 
--- src/com/phoenices/chapters/WelcomeActivity.java
+++ src/com/phoenices/chapters/WelcomeActivity.java
@@ -2,15 +2,60 @@ package com.phoenices.chapters;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends Activity {
-
+	
+	private Button bNewGame;
+	private Button bResume;
+	private Button bSettings;
+	
+	private View.OnClickListener ListenerNewGame = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Intent intentState = new Intent(WelcomeActivity.this, ResumeActivity.class);
+			
+			startActivity(intentState);
+			return;
+		}
+	};
+	
+	private View.OnClickListener ListenerResume = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Intent intentState = new Intent(WelcomeActivity.this, ResumeActivity.class);
+			
+			startActivity(intentState);
+			return;
+		}
+	};
+	
+	private View.OnClickListener ListenerSettings = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			return;			
+		}
+	};
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_welcome);
+        
+		bNewGame = (Button) findViewById(R.id.btn_newgame);
+		bResume = (Button) findViewById(R.id.btn_resume);
+		bSettings = (Button) findViewById(R.id.btn_settings);
+		
+		bNewGame.setOnClickListener(ListenerNewGame);
+		bResume.setOnClickListener(ListenerResume);
+		bSettings.setOnClickListener(ListenerSettings);
     }
+    
+    
 
 
     @Override
